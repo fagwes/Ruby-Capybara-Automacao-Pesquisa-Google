2546724d5a831ca3d7f3a3f731aed904309f483d
fagwes <109398136+fagwes@users.noreply.github.com>
2023-04-04 12:54:17 -0300

    Update register_steps.rb

--- register_steps.rb
+++ register_steps.rb
@@ -1,21 +1,17 @@
 Given("Cliquei em Entrar") do
     visit "https://www.google.com.br/"
     page.windows[0].maximize
-    
 end
 
 When("Eu clico nos campos obrigatórios") do
-
 find(:xpath,'/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').set('ferrari f40')
 find(:xpath,'//*[@id="jZ2SBf"]/div[1]/span').click
 find(:xpath,'//*[@id="tsf"]/div[1]/div[1]/div[2]/div/div[2]/input').set('lamborghini')
 click_button 
 click_link 'Automobili Lamborghini - Official Website | Lamborghini.com'
-
 end
 
 Then("pesquisa realizada com sucesso") do
 save_screenshot
-
 end
 
